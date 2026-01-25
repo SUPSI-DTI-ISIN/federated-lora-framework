d57f2c8ef24b2b22042c28cf550d488be1abd068
Luca Fanto <luca.fanto@supsi.ch>
2026-01-25 23:45:48 +0100

    wip: Work in Progress

---
+++ institute/federated-learning-client/src/federated_learning_client/clients/schemas/__init__.py
@@ -0,0 +1,9 @@
+from federated_learning_client.clients.schemas.document_dto import DocumentDTO
+from federated_learning_client.clients.schemas.section_dto import SectionDTO
+
+__all__ = [
+    'DocumentDTO',
+    'SectionDTO'
+]
+
+__version__ = "1.0.0"
